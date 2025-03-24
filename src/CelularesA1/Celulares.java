afe6f3183ff057f9113a475d4bbe0e011a7edc5e
javabetatester <birisesports@gmail.com>
2025-03-23 22:09:10 -0300

    Desafio completo Celular + POO's

--- src/CelularesA1/Celulares.java
+++ src/CelularesA1/Celulares.java
@@ -1,4 +1,39 @@
-package CelularesA1;
+package ConceitoA1;
 
 public class Celulares {
+    public static void main(String[] args) {
+        // Criando o primeiro celular
+        Celular c1 = new Celular();
+        c1.modeloCelular = "Galaxy S22";
+        c1.marcaCelular = "Samsung";
+        c1.corCelular = "Preto";
+        c1.telaCelular = true;
+        c1.bateriaCelular = true;
+        c1.verCarga(); // Vai definir a carga como 100%
+
+        // Mostrar status do celular
+        c1.statusCelular();
+
+        // Testando os métodos
+        c1.verMensagem();
+        c1.verBateria();
+
+        System.out.println("\n--- Segundo celular ---\n");
+
+        // Criando o segundo celular
+        Celular c2 = new Celular();
+        c2.modeloCelular = "iPhone 14";
+        c2.marcaCelular = "Apple";
+        c2.corCelular = "Branco";
+        c2.telaCelular = false;
+        c2.bateriaCelular = false;
+        c2.verCarga(); // Vai definir a carga como 0%
+
+        // Mostrar status do celular
+        c2.statusCelular();
+
+        // Testando os métodos
+        c2.verMensagem();
+        c2.verBateria();
+    }
 }
--- src/ConceitoA1/Celular.java
+++ src/ConceitoA1/Celular.java
@@ -1,4 +1,43 @@
 package ConceitoA1;
 
 public class Celular {
+    public String modeloCelular;
+    public String marcaCelular;
+    public String corCelular;
+    public boolean telaCelular;
+    public boolean bateriaCelular;
+    public String cargaCelular;
+
+    public void statusCelular() {
+        System.out.println("------- STATUS DO CELULAR -------");
+        System.out.println("Modelo: " + this.modeloCelular);
+        System.out.println("Marca: " + this.marcaCelular);
+        System.out.println("Cor: " + this.corCelular);
+        System.out.println("Tela: " + this.telaCelular);
+        System.out.println("Bateria: " + this.bateriaCelular);
+        System.out.println("Carga: " + this.cargaCelular);
+    }
+    public void verMensagem(){
+        if (this.telaCelular == true) {
+            System.out.println("Consigo ver a mensagem, bacana!");}
+        else {
+            System.out.println("Tesla desligada...");}
+    }
+
+    public void verBateria(){
+        if (this.bateriaCelular == true) {
+            System.out.println("Consigo ver a bateria!");
+        } else {
+            System.out.println("Infelizmente não tem bateria.");
+        }
+    }
+
+    public void verCarga(){
+        if (this.bateriaCelular == true) {
+            this.cargaCelular = ("100%");
+        } else {
+            this.cargaCelular = ("0%");
+        }
+    }
+
 }
